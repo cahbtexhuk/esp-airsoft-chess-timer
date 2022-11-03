cb78a2090ef4ffc5ae576990fadbf487a398e128
IgorR <i.rehacevs@gmail.com>
2022-11-03 18:12:35 +0000

    initial commit, added OTA for test

---
+++ esp8266-chess-timer/src/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+
+enum STATE { IDLE, RESET, T_BLUE, T_RED };
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+
+const char* ssid = "5G Covid-19";
+const char* password = "samplepass";
+
+AsyncWebServer server(80);
+
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting...");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP8266.");
+  });
+
+  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
+
+    // put your setup code here, to run once:
+
+  // setup timers
+
+  // set up buttons
+
+  // set up wifi
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int state; //tis a state machine, after all
+}
+
+
